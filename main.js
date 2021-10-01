fba4261086f7083551330d4548fde12a363d5286
unknown <florencia.fernandezb@davinci.edu.ar>
2021-10-01 02:18:05 -0300

    fix last details and add more styles

--- main.js
+++ main.js
@@ -1,110 +1,134 @@
-'use-strict';
-let GOOGLE_API_KEY = "AIzaSyBo6hfkrJYKHFbtGrPuhcoQldA26su_6mA"
+"use-strict";
+let GOOGLE_API_KEY = "AIzaSyBo6hfkrJYKHFbtGrPuhcoQldA26su_6mA";
 
 function createWeatherDetail(data) {
-    console.log(data)
-    let container = document.getElementById("weatherDetail");
+  console.log(data);
+  let container = document.getElementById("weatherDetail");
 
-    let div = document.createElement('div');
-    div.setAttribute('id', 'details');
+  let div = document.createElement("div");
+  div.setAttribute("id", "details");
 
-    let img = document.createElement('img')
-    img.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+  let img = document.createElement("img");
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
-    let temp = document.createElement('p');
-    temp.setAttribute('class', 'tempDetail');
+  let maxTemp = document.createElement("p");
+  maxTemp.setAttribute("class", "tempDetail");
 
-    let maxTemp = document.createElement('p');
-    maxTemp.setAttribute('class', 'tempDetail');
+  let minTemp = document.createElement("p");
+  minTemp.setAttribute("class", "tempDetail");
 
-    let minTemp = document.createElement('p');
-    minTemp.setAttribute('class', 'tempDetail');
+  let humidity = document.createElement("p");
+  humidity.setAttribute("class", "tempDetail");
 
-    let humidity = document.createElement('p');
-    humidity.setAttribute('class', 'tempDetail');
+  let feelsLike = document.createElement("p");
+  feelsLike.setAttribute("class", "tempDetail");
 
-    let feelsLike = document.createElement('p');
-    feelsLike.setAttribute('class', 'tempDetail');
+  let pressure = document.createElement("p");
+  pressure.setAttribute("class", "tempDetail");
 
-    let pressure = document.createElement('p');
-    pressure.setAttribute('class', 'tempDetail');
+  let windSpeed = document.createElement("p");
+  windSpeed.setAttribute("class", "tempDetail");
 
-    let windSpeed = document.createElement('p');
-    windSpeed.setAttribute('class', 'tempDetail');
+  maxTemp.innerHTML = `Maximum temperature: <span>${data.main.temp_max}°</span>`;
+  minTemp.innerHTML = `Minimum temperature: <span>${data.main.temp_min}°</span>`;
+  humidity.innerHTML = `Humidity: <span>${data.main.humidity}°</span>`;
+  feelsLike.innerHTML = `Feels like: <span>${data.main.feels_like}°</span>`;
+  pressure.innerHTML = `Prssure: <span>${data.main.pressure}°</span>`;
+  windSpeed.innerHTML = `Wind speed: <span>${data.wind.speed} km/h</span>`;
 
-    temp.textContent = `Temperatura: ${data.main.temp}°`
-    maxTemp.textContent = `Temperatura máxima: ${data.main.temp_max}°`
-    minTemp.textContent = `Temperatura mínima: ${data.main.temp_min}°`
-    humidity.textContent = `Humedad: ${data.main.humidity}°`
-    feelsLike.textContent = `Sensación térmica: ${data.main.feels_like}°`
-    pressure.textContent = `Presión atmosférica: ${data.main.pressure}°`
-
-    div.append(img, temp, maxTemp, minTemp, humidity, feelsLike, pressure, windSpeed)
-    container.appendChild(div)
-}
-
-function setupDate(data) {
-    let date = new Date()
-    let container = document.getElementById("weather");
-    
-    let div = document.createElement('div');
-    div.setAttribute('id', 'date');
-    div.setAttribute('class', 'col-12');
-
-    let today = document.createElement('p')
-    today.textContent = date.toDateString()
-
-    let weatherDescription = document.createElement('p')
-    weatherDescription = data.description
-
-    div.append(today, weatherDescription)
-    container.appendChild(div)
-}
-
-function createMap(data) {
-    let container = document.getElementById("weather");
-    
-    let div = document.createElement('div');
-    div.setAttribute('id', 'map');
-    div.setAttribute('class', 'col-12');
-
-    let iframe = document.createElement('iframe')
-    iframe.width = "450"
-    iframe.height = "250"
-    iframe.frameborder = "0"
-    iframe.src = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${data.lat},${data.lon}` 
-
-    div.appendChild(iframe)
-    container.appendChild(div)
+  div.append(img, maxTemp, minTemp, humidity, feelsLike, pressure, windSpeed);
+  container.appendChild(div);
 }
 
 function setupCity(data) {
-    let container = document.getElementById("weather");
-    
-    let div = document.createElement('div');
-    div.setAttribute('id', 'city');
+  let container = document.getElementById("weather-row");
 
-    let temperature = document.createElement('p')
-    temperature.textContent = `Temperatura: ${data.main.temp}°`
+  let div = document.createElement("div");
+  div.setAttribute("id", "city");
+  div.setAttribute("class", "col-12");
 
-    let city = document.createElement('h2')
-    city.textContent = data.name
+  let temperature = document.createElement("p");
+  temperature.textContent = `Temperature: ${data.main.temp}°`;
 
-    div.append(temperature, city)
-    container.appendChild(div)
+  let city = document.createElement("h2");
+  city.textContent = data.name;
+
+  div.append(city, temperature);
+  container.appendChild(div);
+}
+
+function setupDate(data) {
+  let date = new Date();
+  let container = document.getElementById("weather-row");
+
+  let div = document.createElement("div");
+  div.setAttribute("id", "date");
+  div.setAttribute("class", "col-12");
+
+  let today = document.createElement("p");
+  today.textContent = date.toDateString();
+
+  let weatherDescription = document.createElement("p");
+  weatherDescription.textContent = data.description;
+
+  div.append(weatherDescription, today);
+  container.appendChild(div);
+}
+
+function createMap(data) {
+  let container = document.getElementById("weather");
+
+  let div = document.createElement("div");
+  div.setAttribute("id", "map");
+  div.setAttribute('class', 'row')
+
+  let iframe = document.createElement("iframe");
+  iframe.setAttribute('class', 'col-12 p-0')
+  iframe.frameborder = "0";
+  iframe.src = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_API_KEY}&q=${data.lat},${data.lon}`;
+
+  div.appendChild(iframe);
+  container.appendChild(div);
 }
 
 function deleteLastSearch() {
-    let divDetails = document.getElementById("details")
-    let divDate = document.getElementById("date")
-    let divCity = document.getElementById("city")
-    if(divDetails && divDate && divCity) {
-        divDetails.remove();
-        divDate.remove();
-        divCity.remove()
-    }
+  let divDetails = document.getElementById("details");
+  let divDate = document.getElementById("date");
+  let divCity = document.getElementById("city");
+  let divMap = document.getElementById("map");
+  if (divDetails && divDate && divCity && divMap) {
+    divDetails.remove();
+    divDate.remove();
+    divCity.remove();
+    divMap.remove();
+  }
 }
 
-function saveResults(result) {
-    localStorage.setItem('responseWeather', JSON.stringify(result))
+function setBackgroundColor(weather) {
+  let thunderstorm = "#272838";
+  let drizzle = "#a5a5a5";
+  let snow = "#acbbce";
+  let clear = "#00b4d8";
+  let clouds = '#cccccc';
+  let body = document.getElementById("body");
+
+  switch (weather) {
+    case "Clouds":
+      body.style.backgroundColor = clouds;
+      break;
+    case "Thunderstorm":
+      body.style.backgroundColor = thunderstorm;
+      break;
+    case "Drizzle":
+    case "Rain":
+      body.style.backgroundColor = drizzle;
+      break;
+    case "Snow":
+      body.style.backgroundColor = snow;
+      break;
+    case "Clear":
+      body.style.backgroundColor = clear;
+      break;
+  }
+  console.log(weather);
 }
--- services/apiCall.js
+++ services/apiCall.js
@@ -1,14 +1,14 @@
 let API_KEY = "db2352834ea4c64b2aa0d35e4b49e033";
-let API_TOMTOM = "ezA7nvGm1PkNwgGdQfyS8SI2tC5GuRPg"
 let degrees = document.getElementById('degrees')
 
 const lastSearch = JSON.parse(localStorage.getItem("responseWeather"));
 
 if (lastSearch != null) {
     createWeatherDetail(lastSearch);
+    setupCity(lastSearch)
     setupDate(lastSearch.weather[0])
     createMap(lastSearch.coord)
-    setupCity(lastSearch)
+    setBackgroundColor(lastSearch.weather[0].main)
 }
 
 function getCity() {
@@ -25,12 +25,17 @@ function getApiResponse(city, degrees) {
         deleteLastSearch()
         console.log(data)
         createWeatherDetail(data)
+        setupCity(data)
         setupDate(data.weather[0])
         createMap(data.coord)
-        setupCity(data)
+        setBackgroundColor(data.weather[0].main)
         saveResults(data)
     })
     .catch(function(error){
         console.log("Hubo un problema con la peticion Fetch" + error.message)
     })
+}
+
+function saveResults(result) {
+    localStorage.setItem('responseWeather', JSON.stringify(result))
 }
